382cf44204e4403ccccbc6c0dd24de875b0d7de0
bhavyakaria <bhavyakaria@yahoo.in>
2020-08-16 20:40:59 +0530

    added project setup

---
+++ magic-strings/projects/magic-string/src/lib/magic-string.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bk-magic-string',
+  template: `
+    <p>
+      magic-string works!
+    </p>
+  `,
+  styles: []
+})
+export class MagicStringComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
