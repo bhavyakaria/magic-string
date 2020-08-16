46db08f2f14787e69d6ee6d3fc404c44c20eaa30
bhavyakaria <bhavyakaria@yahoo.in>
2020-08-16 21:18:48 +0530

    completed word highlight code

--- magic-strings/projects/magic-string/src/lib/magic-string.component.ts
+++ magic-strings/projects/magic-string/src/lib/magic-string.component.ts
@@ -1,19 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'bk-magic-string',
   template: `
-    <p>
-      magic-string works!
-    </p>
+    <input type="text" (keydown.enter)="markText($event.target.value)">
+    <div #content [hidden]="true">
+      <ng-content></ng-content>
+    </div>
+    <div [innerHTML]="controlledContent"></div>
   `,
-  styles: []
+  styles: [`.mark{background-color: yellow}`],
+  encapsulation: ViewEncapsulation.None
 })
 export class MagicStringComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('content', null) content: ElementRef;
+
+  originalContent: string;
+  controlledContent: string;
+
 
   ngOnInit() {
+    this.controlledContent = this.originalContent = this.content.nativeElement.textContent;
+  }
+
+  markText(value: string) {
+    this.controlledContent = this.originalContent;
+    this.controlledContent = this.originalContent.replace(new RegExp(value, 'g'), `<span class="mark">${value}</span>`);
+    console.log(value);
   }
 
 }
